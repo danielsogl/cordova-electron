5f41c06c57c5b229462ac5d90aa6f34e6ff65b2e
Ibby Hadeed <ibby93@gmail.com>
2017-06-02 15:13:56 -0400

    add dist files

---
+++ dist/create.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const path = require('path'), argv = require('nopt')({
+    help: Boolean,
+    cli: Boolean,
+    shared: Boolean,
+    link: Boolean,
+}, { d: '--verbose' });
+if (argv.help || argv.remain.length === 0) {
+    // TODO show usage info
+    process.exit(1);
+}
